374110923e5c46658a7399cabfdf3f8e1b7c58e3
BLANK-TH <blankdev.th@gmail.com>
2021-05-28 11:42:47 -0400

    Add app index

--- subpages/app.py
+++ subpages/app.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect
 from subpages.apps.collarname import collarname
+from pathlib import Path
+from os import getcwd
 
 app = Blueprint("app", __name__, static_folder="static", template_folder="template")
 app.register_blueprint(collarname, url_prefix="/collar-name")
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("app/index.html", pth="app")
+    if request.method == "POST":
+        p = request.form["passphrase"]
+        if not (Path(getcwd() + "/templates/app/privateindex") / (p + ".html")).is_file() or "/" in p:
+            flash("Invalid Passphrase", 'warning')
+            return redirect(request.url)
+        return render_template("app/index.html", pth="app", rp={"app":"Apps"}, private="app/privateindex/{}.html"
+                               .format(p))
+    else:
+        return render_template("app/index.html", pth="app", rp={"app":"Apps"})
