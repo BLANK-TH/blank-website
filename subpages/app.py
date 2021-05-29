f7d3bb0d52ef510cc1b32886426f6fc3d007de3b
BLANK-TH <blankdev.th@gmail.com>
2021-05-29 14:55:29 -0400

    add tc app

--- subpages/app.py
+++ subpages/app.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-from subpages.apps import *
-from pathlib import Path
+from datetime import timedelta
+from math import floor, ceil
 from os import getcwd
+from pathlib import Path
+
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
+from subpages.apps import *
 
 app = Blueprint("app", __name__, static_folder="static", template_folder="template")
 app.register_blueprint(collarname, url_prefix="/collar-name")
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -18,4 +23,30 @@ def index():
     elif request.method == "POST":
         return redirect(url_for("app.index", catpass=request.form["passphrase"]))
     else:
-        return render_template("app/index.html", rp={"app":"Apps"})
+        return render_template("app/index.html", rp={"app": "Apps"})
+
+
+@app.route("/time-calculator", methods=["GET", "POST"])
+def time_calculator():
+    def time_calc(multiplier):
+        return timedelta(seconds=256 * (multiplier - 1) + 128)
+
+    if request.method == "POST":
+        f = {i: (int(j) if ["hour", "minute", "second"] else j) for i, j in request.form.items()}
+        seconds = f["hour"] * 3600 + f["minute"] * 60 + f["second"]
+        if seconds == 0:
+            flash("Time can't be 0", 'warning')
+            return redirect(request.url)
+        cm = (seconds - 128) / 256
+        cm += 1
+        if cm.is_integer():
+            return render_template("app/time-calculator.html", data={"exact": cm,
+                                                                     "time": str(timedelta(seconds=seconds))})
+        else:
+            lower = floor(cm)
+            higher = ceil(cm)
+            return render_template("app/time-calculator.html",
+                                   data={"lower": lower, "higher": higher, "lower_time": str(timedelta(seconds=lower)),
+                                         "higher_time": time_calc(higher), "target": str(timedelta(seconds=seconds))})
+    else:
+        return render_template("app/time-calculator.html")
