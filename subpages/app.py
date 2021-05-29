c841c5c2c37a51715a9014204feeb158103e1b0c
BLANK-TH <blankdev.th@gmail.com>
2021-05-28 21:40:38 -0400

    allow for headers in app secret index

--- subpages/app.py
+++ subpages/app.py
@@ -8,12 +8,14 @@ app.register_blueprint(collarname, url_prefix="/collar-name")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
+    if "catpass" in request.args:
+        p = request.args["catpass"]
+    elif request.method == "POST":
         p = request.form["passphrase"]
-        if not (Path(getcwd() + "/templates/app/privateindex") / (p + ".html")).is_file() or "/" in p:
-            flash("Invalid Passphrase", 'warning')
-            return redirect(request.url)
-        return render_template("app/index.html", pth="app", rp={"app":"Apps"}, private="app/privateindex/{}.html"
-                               .format(p))
     else:
         return render_template("app/index.html", pth="app", rp={"app":"Apps"})
+    if not (Path(getcwd() + "/templates/app/privateindex") / (p + ".html")).is_file() or "/" in p:
+        flash("Invalid Passphrase", 'warning')
+        return redirect(request.url)
+    return render_template("app/index.html", pth="app", rp={"app": "Apps"}, private="app/privateindex/{}.html"
+                           .format(p))
